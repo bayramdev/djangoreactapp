49f454730dd42ecfe1adafb5094d134eb18a6467
bayramdev <48856944+bayramdev@users.noreply.github.com>
2021-12-06 02:32:58 +0300

    feat(web): create logo component

--- web/src/components/Logo.js
+++ web/src/components/Logo.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Logo() {
   return (
-    <img
-      src="/testproof-logo-label.png"
-      alt="Website Logo"
-      className="absolute left-8 top-8 w-44 invisible lg:visible"
-    />
+    <Link to="/" className="absolute left-8 top-8 w-36 invisible lg:visible">
+      <img
+        src="/testproof-logo-label.png"
+        alt="Website Logo"
+        className="w-full h-full"
+      />
+    </Link>
   );
 }
 
