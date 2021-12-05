ae59e093bb79904160f729a99e62aeae56218ce1
bayramdev <48856944+bayramdev@users.noreply.github.com>
2021-12-05 03:15:07 +0300

    feat(web): implement profile page design

--- web/src/pages/Profile.js
+++ web/src/pages/Profile.js
@@ -1,5 +1,73 @@
+import Logo from "../components/Logo";
+
 function Profile() {
-  return <div>profile page</div>;
+  return (
+    <main className="form-page">
+      <Logo />
+      <form className="user-form max-w-xl">
+        <h1 className="form-header">Edit Profile</h1>
+
+        <section className="flex flex-col gap-2">
+          <section className="flex flex-col md:flex-row gap-2">
+            <label className="form-label flex flex-col flex-1">
+              First Name
+              <input
+                name="first-name"
+                type="text"
+                placeholder="First Name"
+                className="form-input"
+              />
+            </label>
+            <label className="form-label flex flex-col flex-1">
+              Last Name
+              <input
+                name="last-name"
+                type="text"
+                placeholder="Last Name"
+                className="form-input"
+              />
+            </label>
+          </section>
+
+          <label className="form-label flex flex-col">
+            Email
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              className="form-input"
+            />
+          </label>
+
+          <label className="form-label flex flex-col">
+            Username
+            <input
+              name="username"
+              type="text"
+              placeholder="Username"
+              className="form-input"
+            />
+          </label>
+
+          <label className="form-label flex flex-col">
+            Verify Password
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              className="form-input"
+            />
+          </label>
+        </section>
+
+        <input
+          type="submit"
+          value="SUBMIT"
+          className="form-submit px-4 w-full"
+        />
+      </form>
+    </main>
+  );
 }
 
 export default Profile;
