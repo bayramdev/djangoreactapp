cfc9e07d06e60a996dd9b260a8f450eaf22c2e7c
bayramdev <48856944+bayramdev@users.noreply.github.com>
2021-12-05 03:10:39 +0300

    feat(web): update route management

--- web/src/App.js
+++ web/src/App.js
@@ -3,15 +3,19 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="profile" element={<Profile />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="edit-profile" element={<Profile />} />
+        <Route path="reset-password" element={<ResetPassword />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
