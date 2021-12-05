4273d1b1e68b27e2b32e2ebe4952e0e293689533
bayramdev <48856944+bayramdev@users.noreply.github.com>
2021-12-06 02:40:12 +0300

    feat(web): implement home page log out functionality

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,25 +1,39 @@
 import { Link } from "react-router-dom";
+import useAuth from "../auth/useAuth";
 
 function Home() {
+  const { logout } = useAuth();
+
+  const tryLogout = async () => {
+    try {
+      await logout();
+    } catch (err) {
+      console.error({ ...err });
+    }
+  };
+
   return (
-    <main className="bg-form-neutral-medium h-full flex justify-center">
-      <section className="flex flex-col w-full max-w-xl p-4 gap-4">
+    <main className="bg-form-neutral-medium h-full flex justify-center items-center">
+      <section className="flex flex-col w-full max-w-xl p-4 gap-4 text-center">
         <h1 className="form-header">Home Page</h1>
 
         <section className="flex flex-col gap-2">
           <p>This page is WIP. It is quite empty in here...</p>
 
-          <Link to="/edit-profile" className="text-form-primary-light">
+          <Link to="/settings/edit-profile" className="text-form-primary-light">
             You can edit your profile from here
           </Link>
 
-          <Link to="/reset-password" className="text-form-primary-light">
+          <Link
+            to="/settings/reset-password"
+            className="text-form-primary-light"
+          >
             You can reset your password from here
           </Link>
 
-          <Link to="/logout" className="text-form-primary-light">
+          <button className="text-form-primary-light" onClick={tryLogout}>
             You can log out from here
-          </Link>
+          </button>
         </section>
       </section>
     </main>
