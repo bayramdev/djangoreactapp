dcb072d51e2351e4c19770b8a5c9c1f1a0f2a014
bayramdev <48856944+bayramdev@users.noreply.github.com>
2021-12-05 03:11:40 +0300

    feat(web): create logo component

---
+++ web/src/components/Logo.js
@@ -0,0 +1,11 @@
+function Logo() {
+  return (
+    <img
+      src="/testproof-logo-label.png"
+      alt="Website Logo"
+      className="absolute left-8 top-8 w-44 invisible lg:visible"
+    />
+  );
+}
+
+export default Logo;
