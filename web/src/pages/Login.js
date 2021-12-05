ac6b16dc56fb542e3ffa65943dc659473b101671
bayramdev <48856944+bayramdev@users.noreply.github.com>
2021-12-05 03:14:44 +0300

    feat(web): implement login page design

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+import Logo from "../components/Logo";
+
 function Login() {
-  return <div>login page</div>;
+  return (
+    <main className="form-page">
+      <Logo />
+      <form className="user-form max-w-lg">
+        <h1 className="form-header">Welcome to TestProof</h1>
+        <section className="flex flex-col gap-2">
+          <input
+            name="username"
+            type="text"
+            placeholder="Username or Email"
+            className="form-input"
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="form-input"
+          />
+        </section>
+
+        <section className="flex flex-col gap-2">
+          <input type="submit" value="SIGN UP" className="form-submit" />
+          <span className="text-sm text-gray-600 text-center">
+            Don't have an account?{" "}
+            <Link to="/register" className="text-form-primary-light">
+              Sign Up to TestProof
+            </Link>
+          </span>
+        </section>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
