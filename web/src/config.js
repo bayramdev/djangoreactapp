64fbed58a290dde570f220e3d04ce995e29adca2
Bayram <48856944+bayramdev@users.noreply.github.com>
2021-12-08 09:18:12 +0300

    deploy(web): change base url config

--- web/src/config.js
+++ web/src/config.js
@@ -1,4 +1,3 @@
-export const API_BASE_URL =
-  "https://djangoreactapp-demo.herokuapp.com/";
+export const API_BASE_URL = "https://sapien.pythonanywhere.com/";
 
 export const TOKEN_KEY = "access_key";
