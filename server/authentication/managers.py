c5605b0a223f9ca26b1cc8e85b02adace676a283
bayramdev <48856944+bayramdev@users.noreply.github.com>
2021-12-11 20:17:44 +0300

    fix(server): reorder username and email in manager

--- server/authentication/managers.py
+++ server/authentication/managers.py
@@ -3,24 +3,24 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class CustomAccountManager(BaseUserManager):
-    def create_superuser(self, email, username, password, **other_fields):
-        other_fields.setdefault('is_staff', True)
-        other_fields.setdefault('is_superuser', True)
-        other_fields.setdefault('is_active', True)
+    def create_superuser(self, username, email, password, **other_fields):
+        other_fields.setdefault("is_staff", True)
+        other_fields.setdefault("is_superuser", True)
+        other_fields.setdefault("is_active", True)
 
-        if other_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must be assigned to is_staff=True')
-        if other_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must be assigned to is_superuser=True')
+        if other_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must be assigned to is_staff=True")
+        if other_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must be assigned to is_superuser=True")
 
-        return self.create_user(email, username, password, **other_fields)
+        return self.create_user(username, email, password, **other_fields)
 
-    def create_user(self, email, username, password, **other_fields):
+    def create_user(self, username, email, password, **other_fields):
         if not email:
-            raise ValueError(_('You must provide an email address'))
+            raise ValueError(_("You must provide an email address"))
 
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **other_fields)
+        user = self.model(username=username, email=email, **other_fields)
         user.set_password(password)
         user.save()
         return user
